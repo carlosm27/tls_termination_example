7e318eb1dec9cebed06a9b504a1d6be19e7ba28f
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2024-12-20 22:30:16 -0400

    Replace `std` for `boring` and add `http_service`

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,33 @@
+
 use rama::{
     graceful::Shutdown,
-    layer::ConsumeErrLayer,
-    net::forwarded::Forwarded,
-    net::stream::{SocketInfo, Stream},
+    http::{server::HttpServer, Request, Response},
+    layer::{ConsumeErrLayer, GetExtensionLayer},
+    net::{
+        forwarded::Forwarded,
+        stream::SocketInfo,
+        tls::server::{SelfSignedData, ServerAuth, ServerConfig},
+    },
     proxy::haproxy::{
         client::HaProxyLayer as HaProxyClientLayer, server::HaProxyLayer as HaProxyServerLayer,
     },
+    rt::Executor,
     service::service_fn,
     tcp::{
         client::service::{Forwarder, TcpConnector},
         server::TcpListener,
     },
-    tls::boring::server::{ TlsAcceptorData, TlsAcceptorLayer},
+    tls::{
+        boring::server::{TlsAcceptorData, TlsAcceptorLayer},
+        types::SecureTransport,
+    },
     Context, Layer,
-    
 };
-use rama::net::tls::server::{SelfSignedData, ServerAuth, ServerConfig};
 
 // everything else is provided by the standard library, community crates or tokio
 use std::{convert::Infallible, time::Duration};
-use tokio::io::AsyncWriteExt;
 use tracing::metadata::LevelFilter;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
-
-async fn internal_tcp_service_fn<S>(ctx: Context<()>, mut stream: S) -> Result<(), Infallible>
-where
-    S: Stream + Unpin,
-    {
-
-        let client_addr = ctx
-            .get::<Forwarded>()
-            .unwrap()
-            .client_socket_addr()
-            .unwrap();
-        
-        let proxy_addr = ctx.get::<SocketInfo>().unwrap().peer_addr();
-        
-        let payload = format!(
-            "hello client {client_addr}, you were served by tls terminator proxy {proxy_addr}\r\n"
-        );
-
-        let response = format!(
-            "HTTP/1.0 200 ok\r\n\
-                                Connection: close\r\n\
-                                Content-length: {}\r\n\
-                                \r\n\
-                                {}",
-            payload.len(),
-            payload
-        );
-    
-        stream
-            .write_all(response.as_bytes())
-            .await
-            .expect("write to stream");
-    
-        Ok(())
-        
-    }
 
 #[tokio::main]
 async fn main() {
@@ -67,44 +37,71 @@ async fn main() {
             EnvFilter::builder()
                 .with_default_directive(LevelFilter::DEBUG.into())
                 .from_env_lossy(),
-            )
+        )
         .init();
 
     let tls_server_config = ServerConfig::new(ServerAuth::SelfSigned(SelfSignedData::default()));
-    
+
     let acceptor_data = TlsAcceptorData::try_from(tls_server_config).expect("create acceptor data");
 
     let shutdown = Shutdown::default();
 
+    
     shutdown.spawn_task_fn(|guard| async move {
-        let tcp_service = TlsAcceptorLayer::new(acceptor_data).layer(
-            Forwarder::new(([127,0,0,1], 62800)).connector(
+        let tcp_service = (
+            TlsAcceptorLayer::new(acceptor_data).with_store_client_hello(true),
+            GetExtensionLayer::new(|st: SecureTransport| async move {
+                let client_hello = st.client_hello().unwrap();
+                tracing::debug!(?client_hello, "secure connection established");
+            }),
+        )
+            .layer(Forwarder::new(([127, 0, 0, 1], 62801)).connector(
+                // ha proxy protocol used to forwarded the client original IP
                 HaProxyClientLayer::tcp().layer(TcpConnector::new()),
-            ),
-        );
+            ));
 
-        TcpListener::bind("127.0.0.1:63800")
+        TcpListener::bind("127.0.0.1:63801")
             .await
             .expect("bind TCP Listener: tls")
             .serve_graceful(guard, tcp_service)
             .await;
     });
 
+    // create http server
     shutdown.spawn_task_fn(|guard| async {
-        let tcp_service = (ConsumeErrLayer::default(), HaProxyServerLayer::new())
-            .layer(service_fn(internal_tcp_service_fn));
+        let exec = Executor::graceful(guard.clone());
+        let http_service = HttpServer::auto(exec).service(service_fn(http_service));
 
-        TcpListener::bind("127.0.0.1:62800")
+        let tcp_service =
+            (ConsumeErrLayer::default(), HaProxyServerLayer::new()).layer(http_service);
+
+        TcpListener::bind("127.0.0.1:62801")
             .await
-            .expect("bind TCP Listerner: http")
+            .expect("bind TCP Listener: http")
             .serve_graceful(guard, tcp_service)
-            .await;    
+            .await;
     });
 
     shutdown
         .shutdown_with_limit(Duration::from_secs(30))
         .await
         .expect("graceful shutdown");
+}
 
+async fn http_service<S>(ctx: Context<S>, _request: Request) -> Result<Response, Infallible> {
+    
+    let client_addr = ctx
+        .get::<Forwarded>()
+        .unwrap()
+        .client_socket_addr()
+        .unwrap();
+    
+    let proxy_addr = ctx.get::<SocketInfo>().unwrap().peer_addr();
 
-    }
+    Ok(Response::new(
+        format!(
+            "hello client {client_addr}, you were served by tls terminator proxy {proxy_addr}\r\n"
+        )
+        .into(),
+    ))
+}
